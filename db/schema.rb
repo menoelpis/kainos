58eaf258f73e11077622a4fa8b00da8af36e32ae
Daniel Park <menoelpis@gmail.com>
2016-04-13 08:29:17 +0900

    Added PostgreSQL Email Contraint

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160412225027) do
+ActiveRecord::Schema.define(version: 20160412232633) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
