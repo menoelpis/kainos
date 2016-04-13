7d675e7abf26da6a3dcdb0acff95db1d89f4cc5f
Daniel Park <menoelpis@gmail.com>
2016-04-13 10:16:52 +0900

    Implemented Member Search Logic

--- app/controllers/members_controller.rb
+++ app/controllers/members_controller.rb
@@ -1,7 +1,16 @@
 class MembersController < ApplicationController
 
 	def index
-		@members = Member.all.limit(10)
+		if params[:keywords].present?
+			@keywords = params[:keywords]
+			member_search_term = MemberSearchTerm.new(@keywords)
+			@members = Member.where(
+				member_search_term.where_clause,
+				member_search_term.where_args).
+			order(member_search_term.order)
+		else
+			@members = []
+		end
 	end
 
 end
