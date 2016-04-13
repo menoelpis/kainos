4eb637cab73f064c77af79bb672060e6e24a30f0
Daniel Park <menoelpis@gmail.com>
2016-04-13 11:08:47 +0900

    Modified Member Controller for Page Offset

--- app/controllers/members_controller.rb
+++ app/controllers/members_controller.rb
@@ -1,13 +1,16 @@
 class MembersController < ApplicationController
+	PAGE_SIZE = 10
 
 	def index
+		@page = (params[:page] || 0).to_i
 		if params[:keywords].present?
 			@keywords = params[:keywords]
 			member_search_term = MemberSearchTerm.new(@keywords)
 			@members = Member.where(
 				member_search_term.where_clause,
 				member_search_term.where_args).
-			order(member_search_term.order)
+			order(member_search_term.order).
+			offset(PAGE_SIZE * @page).limit(PAGE_SIZE)
 		else
 			@members = []
 		end
